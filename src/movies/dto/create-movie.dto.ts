6afeed4a3d941ff2aadd7eecdfb3d27d4800df33
inhyoe <melon7256@naver.com>
2023-02-22 19:39:07 +0900

    DTO Validation Part2 - geners의 유효성isOptional 타입 추가

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,13 +1,16 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+// https://github.com/typestack/class-validator
 
 export class CreateMovieDto {
-   // npm으로 설치한 class-validator
+	// npm으로 설치한 class-validator
 	@IsString()
 	readonly title: string;
 
 	@IsNumber()
 	readonly year: number;
 
+	@IsOptional()
 	@IsString({ each: true }) // 각각의 배열의 유효성을 검사해줌
 	readonly geners: string[];
+
 } // 내가 바라는 request
