c7d36ebdd85f3de00cd6ac89ef4dbc3db2b161a1
inhyoe <melon7256@naver.com>
2023-02-22 19:52:24 +0900

    모듈과 의존성 주입 - controller와 provider를 바꿨음

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app/app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
+  // app.module은 AppController와 AppProvider만 가지고 있어야함
+  // 그렇기 때문에 MoviesService와 MovivesController를 Movices.module로 옮겨야함
 })
 export class AppModule {}
