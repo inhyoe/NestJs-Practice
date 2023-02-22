e49774b69441e2de35f7d6dbabfc4662108d5d7a
inhyoe <melon7256@naver.com>
2023-02-23 00:13:15 +0900

    Testing getAll and getOne function - Testing Code

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
 	let service: MoviesService;
@@ -19,4 +20,35 @@ describe('MoviesService', () => {
 	it('should be 4', () => {
 		expect(2 + 2).toEqual(4);
 	}); //test code
+
+	describe('getAll', () => {
+		// Unit Test
+		it('should return an array', () => {
+			const result = service.getAll();
+
+			expect(result).toBeInstanceOf(Array);
+		});
+	});
+
+	describe('getOne', () => {
+		it('should return a moive', () => {
+			service.createMovie({
+				// 먼저 createMoive로 Movie 객체를 생성해줘야함.
+				title: 'Test Movie',
+				geners: ['test'],
+				year: 2000,
+			});
+			const movie = service.getOne(1);
+			expect(movie).toBeDefined();
+			expect(movie.id).toEqual(1);
+		});
+		it('Should throw 404 error', () => {
+			try {
+				service.getOne(999);
+			} catch (e) {
+				expect(e).toBeInstanceOf(NotFoundException);
+				expect(e.message).toEqual(`Movie with ID 999 not found`);
+			}
+		});
+	});
 });
