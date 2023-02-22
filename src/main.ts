3a62de9246a4b8c775be7d6f0ca0c9de6cfffc3c
inhyoe <melon7256@naver.com>
2023-02-22 19:03:28 +0900

    [feat] update Validation &  forbindRequest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true, // stranger request를 차단할수 있음
+			transform: true, // 유저가 보낸 것을 유저가 원하는 실제 타입으로 바꿔줌
+		}),
+	); //nest Js에서 제공하는 Pipeline Input값의 유효성을 검사해줌
+	await app.listen(3000);
 }
 bootstrap();
