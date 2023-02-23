77a647abc90af6fa5f6aad6ee8aea0f1f1a5ce2c
inhyoe <melon7256@naver.com>
2023-02-23 09:27:10 +0900

    Module 분리해서 하나의 모듈에 실행되게 하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app/app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
+import { BoardService } from './board/board.service';
+import { BoardsController } from './boards/boards.controller';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [MoviesModule],
-  controllers: [AppController],
-  providers: [],
-  // app.module은 AppController와 AppProvider만 가지고 있어야함
-  // 그렇기 때문에 MoviesService와 MovivesController를 Movices.module로 옮겨야함
+	imports: [MoviesModule, TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+	controllers: [AppController, BoardsController],
+	providers: [BoardService],
+	// app.module은 AppController와 AppProvider만 가지고 있어야함
+	// 그렇기 때문에 MoviesService와 MovivesController를 Movices.module로 옮겨야함
 })
 export class AppModule {}
