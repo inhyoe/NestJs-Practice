c83e84d65c5336ae8f0961d0787dce467df3994b
inhyoe <melon7256@naver.com>
2023-02-23 09:27:28 +0900

    모듈 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app/app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
-import { BoardService } from './board/board.service';
-import { BoardsController } from './boards/boards.controller';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
-	imports: [MoviesModule, TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
-	controllers: [AppController, BoardsController],
-	providers: [BoardService],
+	imports: [
+		BoardsModule,
+		MoviesModule,
+		TypeOrmModule.forRoot(typeORMConfig),
+		BoardsModule,
+	],
+	controllers: [AppController],
+	providers: [],
 	// app.module은 AppController와 AppProvider만 가지고 있어야함
 	// 그렇기 때문에 MoviesService와 MovivesController를 Movices.module로 옮겨야함
 })
