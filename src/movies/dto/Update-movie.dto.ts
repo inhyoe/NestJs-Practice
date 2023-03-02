3c2c92538fe55e5a0f428b42ded937f8101cc840
inhyoe <melon7256@naver.com>
2023-03-02 21:38:12 +0900

    [test] practice nest js

--- src/movies/dto/Update-movie.dto.ts
+++ src/movies/dto/Update-movie.dto.ts
@@ -2,7 +2,8 @@ import { PartialType } from '@nestjs/mapped-types';
 import { IsNumber, IsString } from 'class-validator';
 import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto extends PartialType(CreateMovieDto){ //? 이 기능을 사용하기 위해서는 npm i @nestjs/mapped-types 패키지를 설치해야함
+export class UpdateMovieDto extends PartialType(CreateMovieDto){ 
+                                                   //? 이 기능을 사용하기 위해서는 npm i @nestjs/mapped-types 패키지를 설치해야함
                                                    //? mapped-types는 타입을 변환시키고 사용할 수 있게하는 패키지 DTO를 변환시키는걸 도와줌
                                                    //* PartialType은 베이스타입이 필요함(변환시키려는 타입)
 
--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,4 +1,5 @@
 import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { PrimaryColumn } from 'typeorm';
 // https://github.com/typestack/class-validator
 
 export class CreateMovieDto {
@@ -12,5 +13,4 @@ export class CreateMovieDto {
 	@IsOptional()
 	@IsString({ each: true }) // 각각의 배열의 유효성을 검사해줌
 	readonly geners: string[];
-
 } // 내가 바라는 request
